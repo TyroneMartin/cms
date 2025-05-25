b3a0be7e50d60c6779d08ffa4bf87b76c8f3b615
TyroneMartin <tyrone306@byui.edu>
2025-05-24 19:42:13 -0600

    Document component updated with Angular service functionality

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,27 +1,39 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { DocumentItemComponent } from '../document-item/document-item.component'; 
+import { Component, OnInit } from '@angular/core';
+import { DocumentItemComponent } from '../document-item/document-item.component';
 import { Document } from '../document.model';
 import { CommonModule } from '@angular/common';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-list',
   standalone: true,
   imports: [DocumentItemComponent, CommonModule],
   templateUrl: './document-list.component.html',
-  styleUrl: './document-list.component.css'
+  styleUrl: './document-list.component.css',
 })
-export class DocumentListComponent {
-  @Output() selectedDocumentEvent = new EventEmitter<Document>();
-  
-  documents: Document[] = [
-    new Document('1', 'My Senior Project', 'Summary for my senior project', 'https://example.com/senior-project.pdf', []),
-    new Document('2', 'My SQL Notes', 'Notes from my SQL class', 'https://example.com/sql-notes.pdf', []),
-    new Document('3', 'Software Best Practices', 'Technical requirements and specifications', 'https://example.com/software-best-practices.pdf', []),
-    new Document('4', 'Web Application User Guide', 'User guide for the application', 'https://example.com/web-app-user-guide.pdf', []),
-    new Document('5', 'Professional Readiness', 'Financial breakdown for Q2', 'https://example.com/professional-readiness.pdf', [])
-  ];
+// export class DocumentListComponent {
+export class DocumentListComponent implements OnInit {
+
+  // @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  // documents: Document[] = [
+  //   new Document('1', 'My Senior Project', 'Summary for my senior project', 'https://example.com/senior-project.pdf', []),
+  //   new Document('2', 'My SQL Notes', 'Notes from my SQL class', 'https://example.com/sql-notes.pdf', []),
+  //   new Document('3', 'Software Best Practices', 'Technical requirements and specifications', 'https://example.com/software-best-practices.pdf', []),
+  //   new Document('4', 'Web Application User Guide', 'User guide for the application', 'https://example.com/web-app-user-guide.pdf', []),
+  //   new Document('5', 'Professional Readiness', 'Financial breakdown for Q2', 'https://example.com/professional-readiness.pdf', [])
+  // ];
+
+  documents: Document[] = [];
+
+  constructor(private documentService: DocumentService) {}
+
+  ngOnInit() {
+    this.documents = this.documentService.getDocuments();
+  }
 
   onSelectedDocument(document: Document) {
-    this.selectedDocumentEvent.emit(document);
+    // this.selectedDocumentEvent.emit(document);
+    this.documentService.documentSelectedEvent.emit(document);
   }
 }
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DocumentListComponent } from './document-list/document-list.component';
 import { DocumentDetailComponent } from './document-detail/document-detail.component';
 import { Document } from './document.model';
 import { CommonModule } from '@angular/common';
+import { DocumentService } from './document.service';
 
 @Component({
   selector: 'cms-documents',
   standalone: true,
   imports: [DocumentListComponent, DocumentDetailComponent, CommonModule],
   templateUrl: './documents.component.html',
-  styleUrl: './documents.component.css'
+  styleUrl: './documents.component.css',
 })
-export class DocumentsComponent {
+// export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
+
   selectedDocument!: Document;
-  
+
+  constructor(private documentService: DocumentService) {}
+
+  ngOnInit() {
+    this.documentService.documentSelectedEvent.subscribe(
+      (document: Document) => {
+        this.selectedDocument = document;
+      }
+    );
+  }
+
   onSelectedDocument(document: Document) {
     this.selectedDocument = document;
   }
