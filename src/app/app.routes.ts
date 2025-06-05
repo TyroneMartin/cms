32949d2275760a23e01b314da089427cb6334e12
TyroneMartin <tyrone306@byui.edu>
2025-06-04 23:34:37 -0600

    update changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,27 +3,32 @@ import { DocumentsComponent } from './documents/documents.component';
 import { MessageListComponent } from './messages/message-list/message-list.component';
 import { ContactsComponent } from './contacts/contacts.component';
 
-export const routes: Routes = [
-  { 
-    path: '', 
+export const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/documents',
+    pathMatch: 'full',
+  },
+  {
+    path: 'contacts',
     component: ContactsComponent,
-    title: 'Home' 
-  },
-  { 
-    path: 'documents', 
-    component: DocumentsComponent, 
-    title: 'Documents' 
-  },
-  { 
-    path: 'messages', 
-    component: MessageListComponent, 
-    title: 'Messages' 
-  },
-  { 
-    path: '**', 
-    redirectTo: '' // Redirects to homepage for unknown routes
-  }
-];
+    title: 'Contacts',
 
+  },
+  {
+    path: 'documents',
+    component: DocumentsComponent,
+    title: 'Documents',
+  },
+  {
+    path: 'messages',
+    component: MessageListComponent,
+    title: 'Messages',
+  },
+  {
+    path: '**',
+    redirectTo: '', // Redirects to homepage for unknown routes
+  },
+];
 
 // Documentation on routing: https://angular.dev/tutorials/first-app/10-routing
