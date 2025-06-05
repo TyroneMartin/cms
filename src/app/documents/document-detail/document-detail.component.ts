fc054fe9db5a07a91f28eaddb7d1f5e68da732d4
TyroneMartin <tyrone306@byui.edu>
2025-06-04 19:48:42 -0600

    update

--- src/app/documents/document-detail/document-detail.component.ts
+++ src/app/documents/document-detail/document-detail.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Document } from '../document.model';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'cms-document-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './document-detail.component.html',
   styleUrl: './document-detail.component.css'
 })
