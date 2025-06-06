a8347e79ee8086addce629672d9059224751c2fa
TyroneMartin <tyrone306@byui.edu>
2025-06-06 14:05:09 -0600

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { DocumentsComponent } from './documents/documents.component';
 import { MessageListComponent } from './messages/message-list/message-list.component';
 import { ContactsComponent } from './contacts/contacts.component';
-// import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
-// import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/documents', pathMatch: 'full' },
   { path: 'documents', component: DocumentsComponent },
   { path: 'messages', component: MessageListComponent },
-  { path: 'contacts', component: ContactsComponent }
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'documents/new', component: DocumentEditComponent },
+  { path: 'documents/:id', component: DocumentDetailComponent },
+  { path: 'documents/:id/edit', component: DocumentEditComponent }
 ];
 
 @NgModule({
